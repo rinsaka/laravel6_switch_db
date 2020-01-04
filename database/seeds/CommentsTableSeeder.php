094c90c961773594a38c22519a885e17f532b8ee
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2020-01-04 10:17:35 +0900

    seeder

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -4,13 +4,29 @@ use Illuminate\Database\Seeder;
 
 class CommentsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // 一旦中身を削除する
+    DB::table('comments')->delete();
+
+    DB::table('comments')->insert([
+        'title' => '最初のコメント',
+        'body' => '最初のコメントです！'
+    ]);
+
+    DB::table('comments')->insert([
+        'title' => '2つ目',
+        'body' => '2つ目のコメントです！'
+    ]);
+
+    DB::table('comments')->insert([
+        'title' => '<三個目>のコメント',
+        'body' => 'シーダによってテストデータを設定します．'
+    ]);
+  }
 }
